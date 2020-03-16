34b4001220d16d3f06861236543728cb7bc77892
karthikkalidas <karthik.kalidas@gmail.com>
2020-03-16 13:51:11 -0400

    rechanged to minimal mistakes arghh!

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "SLAM Project",
+        "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third item Here’s a numbered list: First Second Third Python code block: import numpy as np def test_function(x, y): z...","categories": [],
+        "tags": ["estimation","filters"],
+        "url": "http://localhost:4000/slam/",
+        "teaser": null
+      }]
